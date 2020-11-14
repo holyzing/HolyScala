56c112e12fb1c585c1d14ad8a8e136fd4d56801a
holyzing <leilei.hou@xtalpi.com>
2020-11-14 18:02:13 +0800

    spark is coming

---
+++ xtalpi/src/main/python/hello_hdfs.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+
+from krbcontext import krbContext
+# from hdfs.client import InsecureClient
+from hdfs.ext.kerberos import KerberosClient
+
+
+def hdfs_connect_demo():
+    with krbContext(using_keytab=True, principal='houleilei@XTALPI-BJ.COM', keytab_file='./houleilei.client.keytab'):
+        client = KerberosClient('http://hadoop01.stor:50070', hostname_override='hadoop01.stor')
+        # client = InsecureClient('http://hadoop01.stor:50070', user='houleilei')
+        result = client.list('/home/holyzing/')
+        print(type(result), result)
+
+
+if __name__ == '__main__':
+    hdfs_connect_demo()
