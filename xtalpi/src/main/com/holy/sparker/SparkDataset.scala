c8c3768e009cda34991a6daf351fa77adee79a26
holyzing <holyzing@163.com>
2020-12-18 20:37:25 +0800

    dataset base api

--- xtalpi/src/main/com/holy/sparker/SparkDataset.scala
+++ xtalpi/src/main/com/holy/sparker/SparkDataset.scala
@@ -1,6 +1,6 @@
 package com.holy.sparker
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Encoder, SparkSession}
 
 
 object SparkDataset {
@@ -48,6 +48,75 @@ object SparkDataset {
         // Global temporary view is cross-session
         // THINK 可以有多个 SparkSession 但是只能有一个 SparkContext ？
         spark.newSession().sql("SELECT * FROM global_temp.people").show()
+
+        /**
+         * Dataset 不是使用java 原生的序列化 或者 kryo 进行网络间对象的传输，
+         *  While both encoders and standard serialization are responsible for turning an object into bytes,
+         *  encoders are code generated dynamically and use a format that allows Spark to perform many
+         *  operations like filtering, sorting and hashing without deserializing the bytes back into an object.
+         */
+        import spark.implicits._
+        case class Person(name: String, age: Long)
+        val caseClassDS = Seq(Person("Andy", 32)).toDS()
+        caseClassDS.show()
+
+        // Encoders for most common types are automatically provided by importing spark.implicits._
+        val primitiveDS = Seq(1, 2, 3).toDS()
+        primitiveDS.map(_ + 1).collect()        // Returns: Array(2, 3, 4)
+        primitiveDS.map(x => x + 2).collect()
+        // DataFrames can be converted to a Dataset by providing a class. Mapping will be done by name
+
+        val peopleDS = df.as[Person]
+        peopleDS.show()
+
+        val peopleDF = spark.sparkContext
+            .textFile("examples/src/main/resources/people.txt")
+            .map(_.split(","))
+            .map(attributes => Person(attributes(0), attributes(1).trim.toInt))
+            .toDF()
+        // Register the DataFrame as a temporary view
+        peopleDF.createOrReplaceTempView("people")
+        val teenagersDF = spark.sql("SELECT name, age FROM people WHERE age BETWEEN 13 AND 19")
+        // The columns of a row in the result can be accessed by field index
+        teenagersDF.map(teenager => "Name: " + teenager(0)).show()
+        teenagersDF.map(teenager => "Name: " + teenager.getAs[String]("name")).show()
+
+        // No pre-defined encoders for Dataset[Map[K,V]], define explicitly
+        implicit val mapEncoder: Encoder[Map[String, Any]] = org.apache.spark.sql.Encoders.kryo[Map[String, Any]]
+        // Primitive types and case classes can be also defined as
+        // implicit val stringIntMapEncoder: Encoder[Map[String, Any]] = ExpressionEncoder()
+
+        // row.getValuesMap[T] retrieves multiple columns at once into a Map[String, T]
+        teenagersDF.map(teenager => teenager.getValuesMap[Any](List("name", "age"))).collect()
+        // Array(Map("name" -> "Justin", "age" -> 19))
+
+        // -----------------------------------------------------------------------------------
+        import org.apache.spark.sql.Row
+        import org.apache.spark.sql.types._
+
+        // Create an RDD
+        val peopleRDD = spark.sparkContext.textFile("examples/src/main/resources/people.txt")
+
+        // The schema is encoded in a string
+        val schemaString = "name age"  // 字段不确定的，事先无法定义的结构化数据
+
+        // Generate the schema based on the string of schema
+        val fields = schemaString.split(" ")
+            .map(fieldName => StructField(fieldName, StringType, nullable = true))
+        val schema = StructType(fields)
+
+        // Convert records of the RDD (people) to Rows
+        val rowRDD = peopleRDD
+            .map(_.split(","))
+            .map(attributes => Row(attributes(0), attributes(1).trim))
+
+        // Apply the schema to the RDD   NOTE 按顺序映射
+        val peopleDF2 = spark.createDataFrame(rowRDD, schema)
+        peopleDF2.createOrReplaceTempView("people")
+        val results = spark.sql("SELECT name FROM people")
+        // The results of SQL queries are DataFrames and support all the normal RDD operations
+        // The columns of a row in the result can be accessed by field index or by field name
+        results.map(attributes => "Name: " + attributes(0)).show()
         spark.stop()
     }
 }
